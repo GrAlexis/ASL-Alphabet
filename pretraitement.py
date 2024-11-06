1d9e4fc038542fca54239976245988791a62168f
Alexis <alexis.graul@yahoo.fr>
2024-11-06 09:55:12 +0100

    afficher une image oK

--- pretraitement.py
+++ pretraitement.py
@@ -3,19 +3,13 @@ import torch
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
-import tkinter as tk
-from tkinter import filedialog
 
-# Initialiser Tkinter et cacher la fenêtre principale
-root = tk.Tk()
-root.withdraw()  # Ne pas afficher la fenêtre principale
+# Demander à l'utilisateur de spécifier le dossier contenant les images
+dataset_dir = input("Entrez le chemin du dossier contenant les images: ")
 
-# Ouvrir la boîte de dialogue pour choisir un dossier
-dataset_dir = filedialog.askdirectory(title="Sélectionner le dossier contenant les images")
-
-# Vérifier si un dossier a été sélectionné
-if not dataset_dir:
-    print("Aucun dossier sélectionné.")
+# Vérifier si le dossier existe
+if not os.path.isdir(dataset_dir):
+    print("Le dossier spécifié n'existe pas.")
     exit()
 
 # Définir les transformations que vous souhaitez appliquer aux images
@@ -33,7 +27,7 @@ dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 def show_all_images(dataloader):
     """ Affiche toutes les images du dataset dans un seul lot """
     for i, (images, labels) in enumerate(dataloader):
-        imshow(images)
+        imshow(images)  # Afficher le lot d'images
         if i == len(dataloader) - 1:  # Si c'est le dernier lot
             break  # Arrêter après le dernier lot
 
@@ -45,11 +39,13 @@ def show_one_image(image):
 # Fonction pour afficher les images (utilisé par les deux fonctions précédentes)
 def imshow(images):
     """ Affiche les images en utilisant matplotlib """
+    # Convertir le lot d'images (N, C, H, W) en format (N, H, W, C)
     images = images.numpy().transpose((0, 2, 3, 1))  # Convertir en format (N, H, W, C)
+    
     plt.figure(figsize=(10, 10))
     for i in range(images.shape[0]):
         plt.subplot(2, 2, i + 1)  # Disposer les images en 2x2
-        plt.imshow(images[i])
+        plt.imshow(images[i])  # Afficher l'image i
         plt.axis('off')  # Masquer les axes
     plt.show()
 
